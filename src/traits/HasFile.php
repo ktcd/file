f956381c71d922aef8388eb33cb4320d001b4939
Irvan Maulana <me@tukangremot.com>
2020-07-06 11:19:31 +0700

    chore: addFile

--- src/traits/HasFile.php
+++ src/traits/HasFile.php
@@ -2,6 +2,7 @@
 
 namespace Ktcd\File\Traits;
 
+use Ramsey\Uuid\Uuid;
 use Ktcd\File\Models\File as FileModel;
 
 trait HasFile
@@ -26,19 +27,37 @@ trait HasFile
 
     public function file()
     {
-        return $this->morphTo(FileModel::class, 'model', 'model_type', 'model_id')->select([
-            'files.id',
-            'files.name',
-            'files.path',
-            'files.model_id',
-            'files.model_type',
-            'files.type',
-            'files.mime_type',
-            'files.size',
-            'files.created_by',
-            'files.updated_by',
-            'files.created_at',
-            'files.updated_at'
-        ]);
+        return $this->morphOne(FileModel::class, 'model');
+    }
+
+    public function addFile($requestFile, $type = null, $target = null, $data = [])
+    {
+        $this->files()->delete();
+        $this->store($requestFile, $type, $target, $data);
+    }
+
+    private function store($requestFile, $type = null, $target = null, $data = [])
+    {
+        if (!$target) {
+            $target = 'files';
+        }
+        $target = 'files/' . $target;
+
+        $name = $requestFile->getClientOriginalName();
+		$extension = $requestFile->getClientOriginalExtension();
+		$size = $requestFile->getSize();
+        $mimeType = $requestFile->getMimeType();
+        $filename = join('_', [$type, Uuid::uuid4()->toString()]);
+        $path = $target . '/' . $filename . '.' . $extension; 
+
+        $requestFile->move($target, $filename . '.' . $extension);
+
+        $this->file()->create(array_merge([
+            'name' => $name,
+            'path' => $path,
+            'type' => $type,
+            'mime_type' => $mimeType,
+            'size' => $size
+        ], $data));
     }
 }
